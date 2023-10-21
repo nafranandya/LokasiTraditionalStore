f44f714e7b0faffa05e5038002a38bddc6095660
nafranandya <nafranandya@mail.ugm.ac.id>
2023-10-21 18:38:33 +0800

    first commit

---
+++ data/toko.js
@@ -0,0 +1,12 @@
+var toko_pt = {
+    "type": "FeatureCollection",
+    "name": "toko",
+    "crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+    "features": [
+    { "type": "Feature", "properties": { "No": 1, "NomorToko": "Toko 1", "NamaToko": "Toko Rahmadi", "X": -0.421766, "Y": 116.985894 }, "geometry": { "type": "Point", "coordinates": [ 116.985894, -0.421766 ] } },
+    { "type": "Feature", "properties": { "No": 2, "NomorToko": "Toko 2", "NamaToko": "Toko Ikhlas", "X": -0.4212661, "Y": 116.9865808 }, "geometry": { "type": "Point", "coordinates": [ 116.9865808, -0.4212661 ] } },
+    { "type": "Feature", "properties": { "No": 3, "NomorToko": "Toko 3", "NamaToko": "Toko Annes", "X": -0.4200742, "Y": 116.985657 }, "geometry": { "type": "Point", "coordinates": [ 116.985657, -0.4200742 ] } },
+    { "type": "Feature", "properties": { "No": 4, "NomorToko": "Toko 4", "NamaToko": "Toko Sartika", "X": -0.4175937, "Y": 116.9858173 }, "geometry": { "type": "Point", "coordinates": [ 116.9858173, -0.4175937 ] } },
+    { "type": "Feature", "properties": { "No": 5, "NomorToko": "Toko 5", "NamaToko": "Toko Zulfikar", "X": -0.414902, "Y": 116.986129 }, "geometry": { "type": "Point", "coordinates": [ 116.986129, -0.414902 ] } }
+    ]
+    }
